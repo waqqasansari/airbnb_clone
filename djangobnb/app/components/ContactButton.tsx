03a2ca7a81d0f0f62fcefb977b9b611038d7d389
waqqas <waqmid@gmail.com>
2024-09-06 16:19:49 +0530

    landlord and property page

---
+++ djangobnb/app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+  return (
+    <div className="mt-6 py-4 cursor-pointer px-6 bg-airbnb text-white rounded-xl hover:bg-airbnb-dark transition">
+      Contact
+    </div>
+  );
+};
+
+export default ContactButton;
--- djangobnb/app/properties/[id]/page.tsx
+++ djangobnb/app/properties/[id]/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import ResevationSidebar from "@/app/components/Properties/ReservationSidebar";
 
 const PropertyDetailsPage = () => {
   return (
-    <main className="max-w-[1500px] mx-auto px-6">
-      <div className="w-full h-[64vh] overflow-hidden rounded-xl relative">
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
         <Image
           fill
           src="/logo/beach2.webp"
@@ -12,7 +13,7 @@ const PropertyDetailsPage = () => {
         />
       </div>
 
-      <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
         <div className="py-6 pr-6 col-span-3">
           <h1 className="mb-4 text-4xl">Property Name</h1>
           <span className="mb-6 block text-lg text-gray-600">
@@ -21,9 +22,26 @@ const PropertyDetailsPage = () => {
 
           <hr />
 
-          <div className="py-6 flex items-center space-x-4"></div>
+          <div className="py-6 flex items-center space-x-4">
+            <Image
+              src="/pics/profile-pic.png"
+              width={50}
+              height={50}
+              className="rounded-full"
+              alt="The user name"
+            />
+            <p>
+              <strong>Jhon Doe</strong> is your host
+            </p>
+
+            <hr />
+            <p className="mt-6 text-lg">
+              alsg;lasgpwrhgpialsfgn;labnjlnoh oah gnal;fg ag og
+              algh;ldfhglkjahsfgoiuahrgn
+            </p>
+          </div>
         </div>
-        <div>right side</div>
+        <ResevationSidebar />
       </div>
     </main>
   );
